41a2c80cb59d1d3d5d52b4bf3eff63e7856abae1
David <vaddi20758@gmail.com>
2023-06-02 11:13:38 -0400

    1.0.6.2
    - random code

--- demo/src/main/java/GameFrame.java
+++ demo/src/main/java/GameFrame.java
@@ -8,12 +8,24 @@ public class GameFrame extends JFrame
     String ipAddress;
     Player localPlayer;
     Game game;
+
+    
     
     public GameFrame(Player LocalPlayer, String IpAddress) throws IOException, InterruptedException
     {
         localPlayer = LocalPlayer;
         ipAddress = IpAddress;
         game = Client.getMap(ipAddress, LocalPlayer);
+    
+        LocalPlayer=Client.joinGame(ipAddress);
+        if (LocalPlayer.getPlayerID()==1){
+            Client.startGame(IpAddress, LocalPlayer);
+        }
+        
+        for (int i=0;i<game.getBoard();i++){
+
+        }
+        
 
     }
 }
