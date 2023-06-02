a9b92841f4914e6ebcb0132ab73f7d97d91a3510
AudenDavis420 <91392537+AudenDavis420@users.noreply.github.com>
2023-06-02 11:09:45 -0400

    1.0.6.2
    - random code

--- demo/src/main/java/LoadFrame.java
+++ demo/src/main/java/LoadFrame.java
@@ -1,20 +1,27 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.LayoutStyle;
+import javax.swing.SwingConstants;
 
+import java.awt.Color;
+import java.awt.Font;
 public class LoadFrame extends JFrame
 {
     public LoadFrame()
     {
         this.setVisible(true);
-        this.setBounds(50, 50, 200, 200);
+        this.setBounds(0, 0, 500, 500);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setLayout(null);
 
-        JLabel title = new JLabel();
-        title.setBounds(50, 50, 100, 100);
-        title.setText("Clash of Pixels");
+        JLabel title = new JLabel("Clash of Pixels",SwingConstants.CENTER);
+        title.setBounds(0, 70, 500, 30);
+        title.setFont(new Font("SansSerif", Font.ROMAN_BASELINE, 30));
+        title.setForeground(Color.getColor("4AC802"));
         
-
-        this.add(title);
+        
+        this.add(title,SwingConstants.CENTER);
 
 
     }
