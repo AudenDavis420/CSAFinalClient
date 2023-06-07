9c29f6d63bceea5092b7f5b7555f017a938e06cb
David <vaddi20758@gmail.com>
2023-06-07 10:58:47 -0400

    1.0.8
    -code

--- demo/src/main/java/GameFrame.java
+++ demo/src/main/java/GameFrame.java
@@ -4,6 +4,7 @@ import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 import java.awt.event.ActionListener;
 import java.awt.Color;
@@ -31,7 +32,7 @@ public class GameFrame extends JFrame
 
         JFrame frame=new JFrame();
         JButton startGame=new JButton("Start Game?");
-        frame.setBounds(50,50,500,500);
+        frame.setBounds(50,50,500,600);
         frame.add(startGame);
         startGame.setBounds(100,100,100,50);
       
@@ -81,28 +82,63 @@ public class GameFrame extends JFrame
         {
             for(int j=0;j<board[0].length;j++)
             {
-                JComponent landscape=new JComponent(); 
-                    if (board[i][j].getTerrain.equals("grass")){
+                JPanel landscape  = new JPanel();
+                    if (board[i][j].getTerrain().equals("grass")){
                         landscape.setVisible(true);
                         landscape.setBackground(Color.GREEN);
                     }
-                    if (board[i][j].getTerrain=="water"){
+                    if (board[i][j].getTerrain().equals("water")){
                         landscape.setVisible(true);
+
                         landscape.setBackground(Color.BLUE);
                     }
-                    if (board[i][j].getTerrain=="sand"){
+                    if (board[i][j].getTerrain().equals("sand")){
                         landscape.setVisible(true);
                         landscape.setBackground(Color.YELLOW);
                     }
                 
+                area.setBounds(50+i, 50+j, 25,25);
                 area.add(landscape);
+                frame.add(area);
+                area.setVisible(true);
             }
         }
+        JButton move=new JButton("Move");
+        JButton attack=new JButton("attack");
+        JButton end=new JButton("end");
+        move.setBounds(100,500,130,100);
+        attack.setBounds(235,500,130,100);
+        end.setBounds(370,500,130,100);
+        JTextArea sX=new JTextArea("enter start X");
+        JTextArea sY=new JTextArea("enter start Y");
+        JTextArea gX=new JTextArea("enter goal X");
+        JTextArea gY=new JTextArea("enter goal Y");
+        JTextArea moveType=new JTextArea("enter move type");
+        frame.add(sX);
+        frame.add(move);
+        frame.add(attack);
+        frame.add(end);
+        
+        move.addActionListener(new ActionListener(){
 
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               frame.add(sX);
+               frame.add(sY);
+               frame.add(gX);
+               frame.add(gY);
+               frame.add(moveType);
 
+               int sx=Integer.parseInt(sX.getText());
+               int sy=Integer.parseInt(sY.getText());
+               int gx=Integer.parseInt(gX.getText());
+               int gy=Integer.parseInt(gY.getText());
+               String movetype=moveType.getText();
+               MoveRequest move=new MoveRequest(sx,sy,gx,gy,movetype);
 
-
-      
+            }
+            
+        });
 
 
         }
